c50081d6496f3dc7e55159784ae31f4ac27813cd
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-03 19:49:24 -0400

    add setup and instructions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hoohackedme
 CS 6501 (NetSec) project
+
+## Requirements
+- Linux
+
+## Setup
+Run [setup.sh](setup.sh) to download the `rockyou.txt` password list.
---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget -O rockyou.txt.bz2 https://downloads.skullsecurity.org/passwords/rockyou.txt.bz2
+bzip2 -fdk rockyou.txt.bz2
+
+echo "Downloaded and extracted rockyou.txt!"
