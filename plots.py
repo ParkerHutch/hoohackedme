4f1a511c0e03bdccabf7b3d827ec4d55e3684bf4
j-miskill <jcmiskill@gmail.com>
2024-05-06 16:51:39 -0400

    adding the plots into the github

--- plots.py
+++ plots.py
@@ -1,23 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import sys
+
+
+def get_data(filename):
+    is_csv = filename.split(".")[1]
+    assert is_csv == "csv"
+    data = pd.read_csv(filename)
+    return data
 
 
 class Plot:
 
-    def __init__(self, filename):
+    def __init__(self, filename, save_name):
         self.filename = filename
-        self.data = self.get_data(self.filename)
+        self.save_name = save_name
+        self.data = get_data(self.filename)
 
     def plot(self):
-        pass
-
-    def get_data(self, filename):
-        data = {}
-        return data
+        plt.plot(self.data['Guess #'], self.data[' Correct Guesses Total'])
+        # plt.scatter(self.data['Guess #'], self.data[' Correct Guesses Total'])
+        plt.xlabel("Guess #")
+        plt.ylabel("Correct Guesses Total")
+        plt.savefig("model-analysis/%s" % self.save_name)
 
 
 def main():
-    p = Plot()
+    filename = sys.argv[1]
+    save_name = sys.argv[2]
+    p = Plot(filename=filename, save_name=save_name)
     p.plot()
 
 
