61142551af13e2c36b487b8df240443d8ecc8bd7
j-miskill <jcmiskill@gmail.com>
2024-05-07 12:36:00 -0400

    final push to add remaining items (think just the total_data csv)

--- plots.py
+++ plots.py
@@ -18,19 +18,33 @@ class Plot:
         self.save_name = save_name
         self.data = get_data(self.filename)
 
-    def plot(self):
+    def line_plot(self):
         plt.plot(self.data['Guess #'], self.data[' Correct Guesses Total'])
         # plt.scatter(self.data['Guess #'], self.data[' Correct Guesses Total'])
         plt.xlabel("Guess #")
         plt.ylabel("Correct Guesses Total")
         plt.savefig("model-analysis/%s" % self.save_name)
 
+    def bar_plot(self):
+        plt.bar(self.data['Model'], self.data['# Correctly Generated'])
+        plt.ylabel("Number Correct")
+        plt.xlabel("Models")
+        plt.title("Correct Passwords Per Model")
+        plt.savefig("model-analysis/%s" % self.save_name)
+
 
 def main():
     filename = sys.argv[1]
     save_name = sys.argv[2]
+    function = sys.argv[3]
     p = Plot(filename=filename, save_name=save_name)
-    p.plot()
+
+    if function == "l":
+        p.line_plot()
+    elif function == "b":
+        p.bar_plot()
+    else:
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
